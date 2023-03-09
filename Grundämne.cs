47f4b023b8f6ec4adc25eb6a4e09fbdce9a4cfc6
Rebecca Eskekärr <rebecca@eskekarr.se>
2023-03-09 15:15:53 +0100

    publika attribut för grundämnen

---
+++ Grundämne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP_assg_3
+{
+    internal class Grundämne
+    {
+        public string namn;
+        public int Z;
+        public string typ;
+        public double smältpunkt;
+        public double kokpunkt;
+
+
+    }
+}
